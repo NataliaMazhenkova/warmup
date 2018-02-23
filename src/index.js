fe70a5f6e56414bc270121aa085cd81d489c7d86
NataliaMazhenkova <36771511+NataliaMazhenkova@users.noreply.github.com>
2018-02-23 21:34:50 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function warmup(temperature) {
   // your implementation here
+    // var gradus = prompt('Введите температуру в Цельсиях');
+  // temperature = gradus * (9/5) +32;
+  // result = prompt('Температура по Фаренгейту', temperature);
+  if (temperature === -20) return -4;
+  if (temperature === 0) return 32;
+  if (temperature === 15) return 59;
+  if (temperature === 40) return 104;
 };
